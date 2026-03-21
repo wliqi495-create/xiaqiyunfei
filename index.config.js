0c9d7278ea9940d65a621555086824a8edbd87f2
wliqi495-create <wliqi495@gmail.com>
2026-03-21 12:16:52 +0800

    Update index.config.js

--- index.config.js
+++ index.config.js
@@ -175,6 +175,8 @@ var index_config_default = {
       address: "https://t4.jsnzkpg.uk"},
       {name: "📺枫叶",
       address: "https://catbox.n13.club/ai/枫叶音乐.php"},
+    {name: "📺荐片2",
+      address: "http://zhangqun1818.serv00.net:6628/?spider=荐片"},
     {name: "📺影探",
       address: "https://catbox.n13.club/18/影探影视.php"},
     {name: "📺央视",
